a1de05b365cabd9d316b86f615f5e0c23acbdcf2
OscarToro <groovenino@gmail.com>
2013-05-07 18:01:18 +0200

    client has now Operator inner class that helps to recycle some code...yea

--- src/client/Client.java
+++ src/client/Client.java
@@ -16,82 +16,120 @@ import java.net.UnknownHostException;
  */
 public class Client extends Thread {
 
+    //Variables necessary to operate
     DatagramPacket packet;
     static byte[] buffer = new byte[256];
     int port = 7890;
     String fromServer, clientNumber, clientName;
     public InetAddress host;
     boolean youNotGuessTheNumber = true;
+    BufferedReader consoleIn;
+
+
     public Client(int port) throws UnknownHostException{
         this.port = port;
         host = InetAddress.getByName("localhost");
+
     }
 
     public void connect() throws IOException{
+        class Operator {
 
+            BufferedReader bufferedReader;
+            DatagramSocket datagramSocket;
+            DatagramPacket datagramPacket;
+
+            Operator(DatagramSocket datagramSocket, BufferedReader bufferedReader,
+                     DatagramPacket datagramPacket){
+
+                this.bufferedReader = bufferedReader;
+                this.datagramSocket = datagramSocket;
+                this.datagramPacket = datagramPacket;
+
+            }
+
+            public void welcome(){
+
+                System.out.println("Welcome to guess the number");
+                System.out.println("What is your name? :");
+                try {
+                    clientName = bufferedReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            public void prepareNumber(){
+                //prepare the number to send
+                System.out.print("guess a number between 0 and 100 :");
+                try {
+                    clientNumber = bufferedReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //put the number in packet to deliver
+                buffer = clientNumber.getBytes(); //convert String to bytes
+                DatagramPacket outPacket = new DatagramPacket(buffer,buffer.length,host,port);
+
+                //send and receive packages
+                try {
+                    datagramSocket.send(outPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    datagramSocket.receive(datagramPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                fromServer = new String(datagramPacket.getData(),0,datagramPacket.getLength());
+
+            }
+
+            public void informWinner(){
+                System.out.println("Server says: " + fromServer );
+                System.out.println("You won");
+                youNotGuessTheNumber = false;
+            }
+        }
+        //try with resources close the objects after use it, cool stuff from Java 7
         try (DatagramSocket udpClientSocket = new DatagramSocket();
              BufferedReader consoleIn = new BufferedReader(
                      new InputStreamReader(System.in));
         ){
 
             DatagramPacket inPacket = new DatagramPacket(buffer,buffer.length);
+            Operator operator = new Operator(udpClientSocket, consoleIn,inPacket);
             while(youNotGuessTheNumber) {
 
                if(clientName == null){
-                   System.out.println("Welcome to guess the number");
-                   System.out.println("What is your name? :");
-                   clientName = consoleIn.readLine();
-                   System.out.println("your name is :" + clientName);
+                operator.welcome();
 
                }else {
 
                    //first time
                    if(fromServer == null){
+                       operator.prepareNumber();
 
-                       //prepare the number to send
-                       System.out.print("guess a number between 0 and 100 :");
-                       clientNumber = consoleIn.readLine();
-
-                       //put the number in packet to deliver
-                       buffer = clientNumber.getBytes(); //convert String to bytes
-                       DatagramPacket outPacket = new DatagramPacket(buffer,buffer.length,host,port);
-
-                       //send and receive packages
-                       udpClientSocket.send(outPacket);
-                       udpClientSocket.receive(inPacket);
-                       fromServer = new String(inPacket.getData(),0,inPacket.getLength());
-
-                       //if you not guess the number
+                       //if you do not guess the number
                    } else if (fromServer != null && !fromServer.equals("you guessed my number!")) {
-                       //receive
 
+                       //print received info
                        System.out.println("Server says: " + fromServer );
 
                        //Try again and send to the server
                        //prepare the number to send
-                       System.out.print("guess a number between 0 and 100:");
-                       clientNumber = consoleIn.readLine();
+                       operator.prepareNumber();
 
-                       //put the number in packet to deliver
-                       buffer = clientNumber.getBytes(); //convert String to bytes
-                       DatagramPacket outPacket = new DatagramPacket(buffer,buffer.length,host,port);
-
-                       //send and receive packages
-                       udpClientSocket.send(outPacket);
-                       udpClientSocket.receive(inPacket);
-                       fromServer = new String(inPacket.getData(),0,inPacket.getLength());
                    } else {
-                       //receive packet
-                       /*udpClientSocket.receive(inPacket);
-                       fromServer = new String(inPacket.getData(),0,inPacket.getLength());*/
-                       System.out.println("Server says: " + fromServer );
-
-                       System.out.println("You won");
-                       youNotGuessTheNumber = false;
-
+                       operator.informWinner();
                    }
                }
             }
+
 
         } catch (IOException e){
             System.out.println("Socket error. Was not possible to connect with the server");
